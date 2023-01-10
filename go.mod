3c4b9de09a67f1e8335b280b8311a43354fa7ff8
I Made Debrio Amarta <debrioamarta@gmail.com>
2023-01-10 09:39:48 +0700

    membuat depedency

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/MadeDebrio/app-say-hello
+
+go 1.19
+
+require github.com/MadeDebrio/go-say-hello v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	go_say_hello "github.com/MadeDebrio/go-say-hello"
+)
+
+func main() {
+	fmt.Print(go_say_hello.SayHello())
+}
